cd8c70f93f2f0e0ef5410ed5f9db04b68e1cdb75
ekingan <ekingan@gmail.com>
2016-07-08 07:22:24 -0700

    initial commit

---
+++ src/components/Base.jsx
@@ -0,0 +1,18 @@
+//This shows up on every page
+var React = require('react');
+
+var Base = React.createClass({
+	render: function() {
+		
+		return (
+			<div>
+				<h1> Header </h1>
+					{this.props.children}
+				<h2> Footer </h2>
+			</div>
+
+		);
+	}
+});
+
+module.exports = Base;
